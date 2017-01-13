43aa85001aaf883ebc7d3bbafe6616fb287711f4
Chococoin <german.lugo@bitnibs.com>
2017-01-13 03:05:04 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'enter'
+  root 'enter#index'
   get 'about', to: 'root#about'
   get 'index', to: 'root#index'
 end
