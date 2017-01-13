5c2db3bb00feefc5e602dd9744a552ec4cb5a78e
Chococoin <german.lugo@bitnibs.com>
2017-01-13 02:47:51 +0100

    link revised

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'enter/index', to: 'enter#index'
-  get 'enter/about', to: 'enter#about'
+  root 'enter'
+  get 'about', to: 'root#about'
+  get 'index', to: 'root#index'
 end
